cd25622be43eac719606d0a33d4cc3d7300291b9
Jesús Díaz García <jesusdz@gmail.com>
2019-10-27 18:07:57 +0100

    Implemented links and go to action to move player over several rooms.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include <stdint.h>
 
+#define internal static
+
 #define MAX_ROOMS 16
-#define MAX_DOORS (4 * MAX_ROOMS)
+#define MAX_LINKS (4 * MAX_ROOMS)
 #define MAX_ITEMS (4 * MAX_ROOMS)
-#define MAX_ENTITIES (MAX_ROOMS + MAX_DOORS + MAX_ITEMS)
+#define MAX_ENTITIES (MAX_ROOMS + MAX_LINKS + MAX_ITEMS)
 
 #define MAX_ARG_COUNT 8
 #define MAX_ARG_LENGTH 32
@@ -21,6 +24,12 @@ typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
 
+
+////////////////////////////////////////////////////////////////////////
+// String functions
+////////////////////////////////////////////////////////////////////////
+
+internal
 void InputCommandline(char commandline[MAX_COMMANDLINE_LENGTH])
 {
 	printf("\n>> ");
@@ -42,6 +51,7 @@ void InputCommandline(char commandline[MAX_COMMANDLINE_LENGTH])
 	commandline[i] = 0;
 }
 
+internal
 int Tokenize(const char commandline[MAX_COMMANDLINE_LENGTH], char tokens[MAX_ARG_COUNT][MAX_ARG_LENGTH])
 {
 	const char *cptr = commandline;
@@ -65,6 +75,7 @@ int Tokenize(const char commandline[MAX_COMMANDLINE_LENGTH], char tokens[MAX_ARG
 	return tokenId;
 }
 
+internal
 int SameString(const char *string1, const char *string2)
 {
 	const char *str1 = string1;
@@ -91,7 +102,7 @@ int SameString(const char *string1, const char *string2)
 ////////////////////////////////////////////////////////////////////////
 
 struct room;
-struct door;
+struct link;
 struct item;
 
 struct entity
@@ -99,7 +110,7 @@ struct entity
 	entity *Parent = nullptr;
 	
 	room *Room = nullptr;
-	door *Door = nullptr;
+	link *Link = nullptr;
 	item *Item = nullptr;
 
 	const char *Name = "";
@@ -109,7 +120,6 @@ struct entity
 struct player
 {
 	entity * Entity = nullptr;
-	room *CurrentRoom = nullptr;
 };
 
 struct room
@@ -120,12 +130,17 @@ struct room
 struct item
 {
 	entity * Entity = nullptr;
-	const char *location;
+//	const char *location;
+	bool Collectable = false;
 };
 
-struct door
+struct link
 {
 	entity * Entity = nullptr;
+	room * FirstRoom = nullptr;
+	room * SecondRoom = nullptr;
+	const char * FirstRoomLocation = nullptr;
+	const char * SecondRoomLocation = nullptr;
 };
 
 struct world
@@ -141,8 +156,8 @@ struct world
 	item Items[MAX_ITEMS];
 	uint16 ItemCount;
 
-	door Doors[MAX_DOORS];
-	uint16 DoorCount;
+	link Links[MAX_LINKS];
+	uint16 LinkCount;
 };
 
 
@@ -150,6 +165,7 @@ struct world
 // Functions
 ////////////////////////////////////////////////////////////////////////
 
+internal
 entity *CreateEntity(world *World, entity *Parent = nullptr)
 {
 	entity *Result = &World->Entities[World->EntityCount];
@@ -158,12 +174,15 @@ entity *CreateEntity(world *World, entity *Parent = nullptr)
 	return Result;
 }
 
-void CreatePlayer(world *World)
+internal
+void CreatePlayer(world *World, room *InitialRoom)
 {
 	entity *Entity = CreateEntity(World);
-	World->Player.CurrentRoom = 0;
+	World->Player.Entity = Entity;
+	World->Player.Entity->Parent = InitialRoom->Entity;
 }
 
+internal
 room *CreateRoom(world *World)
 {
 	entity *Entity = CreateEntity(World);
@@ -177,19 +196,21 @@ room *CreateRoom(world *World)
 	return Result;
 }
 
-door *CreateDoor(world *World)
+internal
+link *CreateLink(world *World)
 {
 	entity *Entity = CreateEntity(World);
 
-	door *Result = &World->Doors[World->DoorCount];
-	World->DoorCount++;
+	link *Result = &World->Links[World->LinkCount];
+	World->LinkCount++;
 
 	Result->Entity = Entity;
-	Entity->Door = Result;
+	Entity->Link = Result;
 
 	return Result;
 }
 
+internal
 item *CreateItem(world *World)
 {
 	entity *Entity = CreateEntity(World);
@@ -203,13 +224,14 @@ item *CreateItem(world *World)
 	return Result;
 }
 
-entity * FindEntityInRoom(world *World, room *Room, const char *EntityName)
+internal
+entity * FindEntityInEntity(world *World, entity *ParentEntity, const char *EntityName)
 {
-	for (uint16 i = 1; i < World->EntityCount; ++i)
+	for (uint16 i = 0; i < World->EntityCount; ++i)
 	{
 		entity * Entity = &World->Entities[i];
 
-		if (Entity->Parent == Room->Entity)
+		if (Entity->Parent == ParentEntity)
 		{
 			if (SameString(Entity->Name, EntityName))
 			{
@@ -220,79 +242,126 @@ entity * FindEntityInRoom(world *World, room *Room, const char *EntityName)
 	return nullptr;
 }
 
+internal
+entity * FindEntityInCurrentRoom(world *World, const char *EntityName)
+{
+	entity *CurrentRoomEntity = World->Player.Entity->Parent;
+	entity *Result = FindEntityInEntity(World, CurrentRoomEntity, EntityName);
+	return Result;
+}
+
+internal
 entity * FindEntityInInventory(world *World, const char *EntityName)
 {
-	for (uint16 i = 1; i < World->EntityCount; ++i)
-	{
-		entity * Entity = &World->Entities[i];
+	entity *Result = FindEntityInEntity(World, World->Player.Entity, EntityName);
+	return Result;
+}
 
-		if (Entity->Parent == World->Player.Entity)
+internal
+link * FindLinkInRoom(world *World, room *Room, const char *LinkNameOrLocation)
+{
+	for (uint16 i = 0; i < World->LinkCount; ++i)
+	{
+		link * Link = &World->Links[i];
+
+		if (Link->FirstRoom == Room || Link->SecondRoom == Room)
 		{
-			if (SameString(Entity->Name, EntityName))
+			if (SameString(Link->Entity->Name, LinkNameOrLocation) ||
+				(Link->FirstRoom == Room && SameString(Link->SecondRoomLocation, LinkNameOrLocation)) ||
+				(Link->SecondRoom == Room && SameString(Link->FirstRoomLocation, LinkNameOrLocation)))
 			{
-				return &World->Entities[i];
+				return &World->Links[i];
 			}
 		}
 	}
 	return nullptr;
 }
 
-void InitializeWorld(world *World)
-{
-	World->EntityCount = 1;
-	World->RoomCount = 1;
-	World->DoorCount = 1;
-	World->ItemCount = 1;
-
-	CreatePlayer(World);
-
-	room *Room = CreateRoom(World);
-	Room->Entity->Description = "The current room is dark and quiet. You can only see a subtle light coming from a narrow doorway in in the north wall.";
-
-	door *Door = CreateDoor(World);
-	Door->Entity->Name = "doorway";
-	Door->Entity->Description = "It is a scary narrow doorway that leads to a moisty corridor. You can see a subtle light on the other side.";
-
-	item *Item = CreateItem(World);
-	Item->Entity->Parent = Room->Entity;
-	Item->Entity->Name = "dagger";
-	Item->Entity->Description = "It's a small shiny and sharp dagger.";
-	Item->location = "ground";
-
-	World->Player.CurrentRoom = Room;
-
-	Room = CreateRoom(World);
-}
-
-void PrintWelcomeMessage()
-{
-	printf("**********************\n");
-	printf("   Welcome to Zork!\n");
-	printf("**********************\n");
-
-	printf("\n");
-
-	printf("This is a text adventure in the spirit of the classic game Zork.\n");
-	printf("You will advance through the story by means of executing actions.\n");
-	printf("Type 'help' in the commandline to see the available commands.\n");
-	printf("Have fun!\n\n");
-}
-
+internal
 room *GetCurrentRoom(world *World)
 {
-	room *CurrentRoom = World->Player.CurrentRoom;
+	assert(World->Player.Entity != nullptr);
+	assert(World->Player.Entity->Parent != nullptr);
+	assert(World->Player.Entity->Parent->Room != nullptr);
+	room *CurrentRoom = World->Player.Entity->Parent->Room;
 	return CurrentRoom;
 }
 
+internal
+void SetCurrentRoom(world *World, room *Room)
+{
+	assert(Room->Entity != nullptr);
+	assert(World->Player.Entity != nullptr);
+	World->Player.Entity->Parent = Room->Entity;
+}
+
+internal
+room * FindNeighbouringRoom(world *World, const char *LinkNameOrLocation)
+{
+	room *CurrentRoom = GetCurrentRoom(World);
+	link *Link = FindLinkInRoom(World, CurrentRoom, LinkNameOrLocation);
+	if (Link != nullptr)
+	{
+		if (Link->FirstRoom == CurrentRoom)
+		{
+			return Link->SecondRoom;
+		}
+		else
+		{
+			return Link->FirstRoom;
+		}
+	}
+	else
+	{
+		return nullptr;
+	}
+}
+
+int UpdateWorld(world *World, const char *Commandline);
+
+internal
 void UpdateWorld(world *World, uint32 ArgCount, const char *Args[])
 {
 	room *CurrentRoom = GetCurrentRoom(World);
 
-	if (SameString(Args[0], "look"))
+	if (SameString(Args[0], "go"))
+	{
+		if (ArgCount > 1 && ArgCount < 4)
+		{
+			const char *Location = Args[1];
+			
+			if (ArgCount == 3 && SameString(Args[1], "to"))
+			{
+				Location = Args[2];
+			}
+
+			room * NextRoom = FindNeighbouringRoom(World, Location);
+
+			if (NextRoom != nullptr)
+			{
+				SetCurrentRoom(World, NextRoom);
+				UpdateWorld(World, "look");
+			}
+			else
+			{
+				printf("I don't know how to go there.");
+			}
+		}
+		else if (ArgCount >= 4)
+		{
+			printf("Too much information, be more specific.");
+		}
+		else
+		{
+			printf("Where do you want to go?");
+		}
+	}
+	else if (SameString(Args[0], "look"))
 	{
 		if (ArgCount == 1)
 		{
 			printf("%s", CurrentRoom->Entity->Description);
+#if 0
 			for (uint16 i = 1; i < World->ItemCount; ++i)
 			{
 				item * Item = &World->Items[i];
@@ -303,10 +372,11 @@ void UpdateWorld(world *World, uint32 ArgCount, const char *Args[])
 					printf(" There is a %s on the %s.", Entity->Name, Item->location);
 				}
 			}
+#endif
 		}
 		else if (ArgCount == 2)
 		{
-			entity *FoundEntity = FindEntityInRoom(World, CurrentRoom, Args[1]);
+			entity *FoundEntity = FindEntityInCurrentRoom(World, Args[1]);
 			if (FoundEntity == nullptr)
 			{
 				FoundEntity = FindEntityInInventory(World, Args[1]);
@@ -332,9 +402,24 @@ void UpdateWorld(world *World, uint32 ArgCount, const char *Args[])
 			entity *FoundEntity = FindEntityInInventory(World, Args[1]);
 			if (FoundEntity == nullptr)
 			{
-				entity *FoundEntity = FindEntityInRoom(World, CurrentRoom, Args[1]);
-				FoundEntity->Parent = World->Player.Entity;
-				printf("Taken.");
+				entity *FoundEntity = FindEntityInCurrentRoom(World, Args[1]);
+
+				if (FoundEntity != nullptr)
+				{
+					if (FoundEntity->Item->Collectable)
+					{
+						FoundEntity->Parent = World->Player.Entity;
+						printf("Taken.");
+					}
+					else
+					{
+						printf("I cannot take that.");
+					}
+				}
+				else
+				{
+					printf("I cannot find that.");
+				}
 			}
 			else
 			{
@@ -383,11 +468,12 @@ void UpdateWorld(world *World, uint32 ArgCount, const char *Args[])
 	else if (SameString(Args[0], "help"))
 	{
 		printf("You can use the following commands to execute actions:\n");
-		printf("- drop\n");
-		printf("- get\n");
+		printf("- drop <item name>\n");
+		printf("- get <item name>\n");
+		printf("- go [to] <direction or location>\n");
 		printf("- help\n");
 		printf("- inventory\n");
-		printf("- look\n");
+		printf("- look <item name>\n");
 		printf("- quit/exit");
 	}
 	else
@@ -398,6 +484,7 @@ void UpdateWorld(world *World, uint32 ArgCount, const char *Args[])
 	printf("\n");
 }
 
+internal
 void UpdateWorld(world *World, uint32 ArgCount, const char Args[MAX_ARG_COUNT][MAX_ARG_LENGTH])
 {
 	const char *NewArgs[MAX_ARG_COUNT];
@@ -408,6 +495,7 @@ void UpdateWorld(world *World, uint32 ArgCount, const char Args[MAX_ARG_COUNT][M
 	UpdateWorld(World, ArgCount, NewArgs);
 }
 
+internal
 int UpdateWorld(world *World, const char *Commandline)
 {
 	char Tokens[MAX_ARG_COUNT][MAX_ARG_LENGTH];
@@ -427,6 +515,89 @@ int UpdateWorld(world *World, const char *Commandline)
 	}
 
 	return 1;
+}
+
+internal
+void InitializeWorld(world *World)
+{
+	World->EntityCount = 0;
+	World->RoomCount = 0;
+	World->LinkCount = 0;
+	World->ItemCount = 0;
+
+	// Room 1
+
+	room *Room1 = CreateRoom(World);
+	Room1->Entity->Description =
+		"The current room is dark and quiet. "
+		"You can only see a subtle light coming from a narrow doorway in in the north wall.";
+
+	item *Item = CreateItem(World);
+	Item->Collectable = true;
+	Item->Entity->Parent = Room1->Entity;
+	Item->Entity->Name = "dagger";
+	Item->Entity->Description = "It's a small shiny and sharp dagger.";
+	//Item->location = "ground";
+
+
+	// Room 2
+
+	room *Room2 = CreateRoom(World);
+	Room2->Entity->Description =
+		"This room is subtlely lit by a chandelier on an elegant wooden table. "
+		"As you try to inspect details in the penumbra, you can see several paintings on the walls. "
+		"What seems a secret passageway is open leading towards the southern, darker room."
+		"There is big wooden closed door in the right wall. "
+		"An open door to the left leads to another, more illuminated room.";
+
+	Item = CreateItem(World);
+	Item->Entity->Parent = Room2->Entity;
+	Item->Entity->Name = "table";
+	Item->Entity->Description = "It's an old and elegant wooden table.";
+	//Item->location = "ground";
+
+	Item = CreateItem(World);
+	Item->Entity->Parent = Room2->Entity;
+	Item->Entity->Name = "chandelier";
+	Item->Entity->Description = "The chandelier is made of silver. It's candles are burning and emmiting an eerie light.";
+	//Item->location = "table";
+
+	Item = CreateItem(World);
+	Item->Entity->Parent = Room2->Entity;
+	Item->Entity->Name = "door";
+	Item->Entity->Description = "Looks like this door is well closed. I'm going to need a key to open it.";
+	//Item->location = "ground";
+
+
+	// Links between rooms
+
+	link *Link = CreateLink(World);
+	Link->FirstRoom = Room1;
+	Link->FirstRoomLocation = "south";
+	Link->SecondRoom = Room2;
+	Link->SecondRoomLocation = "north";
+	Link->Entity->Name = "passageway";
+	Link->Entity->Description = "It is a scary narrow passageway that leads to another room. You can see a subtle light on the other side.";
+
+
+	// Player
+
+	CreatePlayer(World, Room1);
+}
+
+internal
+void PrintWelcomeMessage()
+{
+	printf("**********************\n");
+	printf("   Welcome to Zork!\n");
+	printf("**********************\n");
+
+	printf("\n");
+
+	printf("This is a text adventure in the spirit of the classic game Zork.\n");
+	printf("You will advance through the story by means of executing actions.\n");
+	printf("Type 'help' in the commandline to see the available commands.\n");
+	printf("Have fun!\n\n");
 }
 
 int main()
